99b525495d412471ac17e1d2a8b4cbd97484f7bd
Jacob-Parcell <jakeparcell14@gmail.com>
2025-04-14 17:17:58 -0700

    Update ai_blocker.js
    Refactored variables and removed whitespace

--- scripts/ai_blocker.js
+++ scripts/ai_blocker.js
@@ -1,12 +1,10 @@
-var ai_stuff = document.getElementsByClassName("YNk70c EjQTId");
+var ai_overview = document.getElementsByClassName("YNk70c EjQTId");
 
-console.log(ai_stuff.length)
-if (ai_stuff.length != 0)
+if (ai_overview.length != 0)
 {
-	while(ai_stuff.length > 0){
+	while(ai_overview.length > 0){
 
-		ai_stuff[0].remove();
+		ai_overview[0].remove();
 	}
 	document.getElementsByClassName("rfiSsc YNk70c q9ZDaf")[0].style.marginBottom = "30px"
-
 }
