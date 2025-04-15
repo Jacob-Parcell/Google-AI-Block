fc025bd89e66887d526ac1c6a6d0dc9cf879d460
Jacob-Parcell <jakeparcell14@gmail.com>
2025-04-14 17:14:49 -0700

    Uploading Project FIles

---
+++ scripts/ai_blocker.js
@@ -0,0 +1,12 @@
+var ai_stuff = document.getElementsByClassName("YNk70c EjQTId");
+
+console.log(ai_stuff.length)
+if (ai_stuff.length != 0)
+{
+	while(ai_stuff.length > 0){
+
+		ai_stuff[0].remove();
+	}
+	document.getElementsByClassName("rfiSsc YNk70c q9ZDaf")[0].style.marginBottom = "30px"
+
+}
